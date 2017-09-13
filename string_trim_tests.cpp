728f817909af54a420fc1d6e7bb9b286ce66bce3
Anthony Williams <anthony@justsoftwaresolutions.co.uk>
2017-09-13 16:16:03 +0100

    Added trim_copy functions

--- string_trim_tests.cpp
+++ string_trim_tests.cpp
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <string>
 #include <cctype>
+#include <type_traits>
 
 template<typename Container>
 void trim_left(Container& s){
@@ -22,6 +23,28 @@ void trim(Container& s){
     trim_right(s);
 }
 
+template<typename Container>
+std::remove_reference_t<Container> trim_copy_left(Container&& s){
+    using string_type=std::remove_reference_t<Container>;
+    auto pos=s.begin();
+    for(auto end=s.end();(pos!=end) && std::isspace(*pos);++pos);
+    return string_type(pos,s.end());
+}
+
+template<typename Container>
+std::remove_reference_t<Container> trim_copy_right(Container&& s){
+    using string_type=std::remove_reference_t<Container>;
+    auto endpos=s.rbegin();
+    for(auto end=s.rend();(endpos!=end) && std::isspace(*endpos);++endpos);
+    return string_type(s.begin(),endpos.base());
+}
+
+template<typename Container>
+std::remove_reference_t<Container> trim_copy(Container&& s){
+    auto temp=trim_copy_left(s);
+    trim_right(temp);
+    return temp;
+}
 
 void trim_empty_string_does_nothing(){
     std::string s;
@@ -77,6 +100,75 @@ void trim_spaces_from_right_only(){
     assert(s==hello);
 }
 
+void trim_copy_empty_string_does_nothing(){
+    std::string orig;
+    auto s=orig;
+    auto s2=trim_copy(s);
+    assert(s.empty());
+    assert(s2.empty());
+}
+
+void trim_copy_non_space_string_does_nothing(){
+    std::string const orig="hello";
+    auto s=orig;
+    auto s2=trim_copy(s);
+    assert(s==orig);
+    assert(s2==orig);
+}
+
+void trim_copy_all_space_string_removes_everything(){
+    std::string const orig="     ";
+    auto s=orig;
+    auto s2=trim_copy(s);
+    assert(s2.empty());
+    assert(s==orig);
+}
+
+void trim_copy_spaces_from_left(){
+    std::string const hello="hello";
+    std::string const orig="     "+hello;
+    auto s=orig;
+    auto s2=trim_copy(s);
+    assert(s2==hello);
+    assert(s==orig);
+}
+
+void trim_copy_spaces_from_right(){
+    std::string const hello="hello";
+    std::string const orig=hello+"     ";
+    auto s=orig;
+    auto s2=trim_copy(s);
+    assert(s2==hello);
+    assert(s==orig);
+}
+
+void trim_copy_spaces_from_both_ends(){
+    std::string const hello="hello";
+    std::string const orig="     "+hello+"   ";
+    auto s=orig;
+    auto s2=trim_copy(s);
+    assert(s2==hello);
+    assert(s==orig);
+}
+
+void trim_copy_spaces_from_left_only(){
+    std::string const hello="hello     ";
+    std::string const orig="     "+hello;
+    auto s=orig;
+    auto s2=trim_copy_left(s);
+    assert(s2==hello);
+    assert(s==orig);
+}
+
+void trim_copy_spaces_from_right_only(){
+    std::string const hello="     hello";
+    std::string const orig=hello+"     ";
+    std::string s=orig;
+    auto s2=trim_copy_right(s);
+    assert(s2==hello);
+    assert(s==orig);
+}
+
 int main(){
     trim_empty_string_does_nothing();
     trim_non_space_string_does_nothing();
@@ -86,4 +178,12 @@ int main(){
     trim_spaces_from_both_ends();
     trim_spaces_from_left_only();
     trim_spaces_from_right_only();
+    trim_copy_empty_string_does_nothing();
+    trim_copy_non_space_string_does_nothing();
+    trim_copy_all_space_string_removes_everything();
+    trim_copy_spaces_from_left();
+    trim_copy_spaces_from_right();
+    trim_copy_spaces_from_both_ends();
+    trim_copy_spaces_from_left_only();
+    trim_copy_spaces_from_right_only();
 }
