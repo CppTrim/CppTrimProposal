58bc4b2cfafb3cb36e65a5a08e7cab93600aeba7
Anthony Williams <anthony@justsoftwaresolutions.co.uk>
2017-09-14 09:21:41 +0100

    Added tests for trimming vectors

--- string_trim_tests.cpp
+++ string_trim_tests.cpp
@@ -1,6 +1,7 @@
 #include "string_trim.hpp"
 #include <assert.h>
 #include <string>
+#include <vector>
 
 void trim_empty_string_does_nothing(){
     std::string s;
@@ -125,6 +126,160 @@ void trim_copy_spaces_from_right_only(){
     assert(s==orig);
 }
 
+void trim_spaces_and_tabs_from_both_ends(){
+    std::string const hello="hello";
+    std::string s="    \t \t \t   \t "+hello+"  \t \t\t\t\t ";
+    trim(s);
+    assert(s==hello);
+}
+
+void trim_empty_vector_does_nothing(){
+    std::vector<char> s;
+    trim(s);
+    assert(s.empty());
+}
+
+void trim_non_space_vector_does_nothing(){
+    std::vector<char> const orig{'h','e','l','l','o'};
+    auto s=orig;
+    trim(s);
+    assert(s==orig);
+}
+
+void trim_all_space_vector_removes_everything(){
+    std::vector<char> s(5,' ');
+    trim(s);
+    assert(s.empty());
+}
+
+void trim_vector_spaces_from_left(){
+    std::vector<char> const hello{'h','e','l','l','o'};
+    std::vector<char> s(5,' ');
+    s.insert(s.end(),hello.begin(),hello.end());
+    trim(s);
+    assert(s==hello);
+}
+
+void trim_vector_spaces_from_right(){
+    std::vector<char> const hello{'h','e','l','l','o'};
+    std::vector<char> spaces(5,' ');
+    std::vector<char> s=hello;
+    s.insert(s.end(),spaces.begin(),spaces.end());
+    trim(s);
+    assert(s==hello);
+}
+
+void trim_vector_spaces_from_both_ends(){
+    std::vector<char> const hello{'h','e','l','l','o'};
+    std::vector<char> spaces(5,' ');
+    std::vector<char> s=spaces;
+    s.insert(s.end(),hello.begin(),hello.end());
+    s.insert(s.end(),spaces.begin(),spaces.end());
+    trim(s);
+    assert(s==hello);
+}
+
+void trim_vector_spaces_from_left_only(){
+    std::vector<char> const hello{'h','e','l','l','o',' ',' ',' '};
+    std::vector<char> spaces(5,' ');
+    std::vector<char> s=spaces;
+    s.insert(s.end(),hello.begin(),hello.end());
+    trim_left(s);
+    assert(s==hello);
+}
+
+void trim_vector_spaces_from_right_only(){
+    std::vector<char> const hello{' ',' ',' ',' ',' ','h','e','l','l','o'};
+    std::vector<char> spaces(5,' ');
+    std::vector<char> s;
+    s.insert(s.end(),hello.begin(),hello.end());
+    s.insert(s.end(),spaces.begin(),spaces.end());
+    trim_right(s);
+    assert(s==hello);
+}
+
+void trim_vector_copy_empty_string_does_nothing(){
+    std::vector<char> orig;
+    auto s=orig;
+    auto s2=trim_copy(s);
+    assert(s.empty());
+    assert(s2.empty());
+}
+
+void trim_vector_copy_non_space_string_does_nothing(){
+    std::vector<char> const orig{'h','e','l','l','o'};
+    auto s=orig;
+    auto s2=trim_copy(s);
+    assert(s==orig);
+    assert(s2==orig);
+}
+
+void trim_vector_copy_all_space_string_removes_everything(){
+    std::vector<char> const orig(6,' ');
+    auto s=orig;
+    auto s2=trim_copy(s);
+    assert(s2.empty());
+    assert(s==orig);
+}
+
+void trim_vector_copy_spaces_from_left(){
+    std::vector<char> const hello{'h','e','l','l','o'};
+    std::vector<char> spaces(5,' ');
+    std::vector<char> orig=spaces;
+    orig.insert(orig.end(),hello.begin(),hello.end());
+    auto s=orig;
+    auto s2=trim_copy(s);
+    assert(s2==hello);
+    assert(s==orig);
+}
+
+void trim_vector_copy_spaces_from_right(){
+    std::vector<char> const hello{'h','e','l','l','o'};
+    std::vector<char> spaces(5,' ');
+    std::vector<char> orig;
+    orig.insert(orig.end(),hello.begin(),hello.end());
+    orig.insert(orig.end(),spaces.begin(),spaces.end());
+    auto s=orig;
+    auto s2=trim_copy(s);
+    assert(s2==hello);
+    assert(s==orig);
+}
+
+void trim_vector_copy_spaces_from_both_ends(){
+    std::vector<char> const hello{'h','e','l','l','o'};
+    std::vector<char> spaces(5,' ');
+    std::vector<char> orig=spaces;
+    orig.insert(orig.end(),hello.begin(),hello.end());
+    orig.insert(orig.end(),spaces.begin(),spaces.end());
+    auto s=orig;
+    auto s2=trim_copy(s);
+    assert(s2==hello);
+    assert(s==orig);
+}
+
+void trim_vector_copy_spaces_from_left_only(){
+    std::vector<char> const hello{'h','e','l','l','o',' ',' ',' '};
+    std::vector<char> spaces(5,' ');
+    std::vector<char> orig=spaces;
+    orig.insert(orig.end(),hello.begin(),hello.end());
+    auto s=orig;
+    auto s2=trim_copy_left(s);
+    assert(s2==hello);
+    assert(s==orig);
+}
+
+void trim_vector_copy_spaces_from_right_only(){
+    std::vector<char> const hello{' ',' ',' ','h','e','l','l','o'};
+    std::vector<char> spaces(5,' ');
+    std::vector<char> orig;
+    orig.insert(orig.end(),hello.begin(),hello.end());
+    orig.insert(orig.end(),spaces.begin(),spaces.end());
+    std::vector<char> s=orig;
+    auto s2=trim_copy_right(s);
+    assert(s2==hello);
+    assert(s==orig);
+}
+
 int main(){
     trim_empty_string_does_nothing();
     trim_non_space_string_does_nothing();
@@ -142,4 +297,21 @@ int main(){
     trim_copy_spaces_from_both_ends();
     trim_copy_spaces_from_left_only();
     trim_copy_spaces_from_right_only();
+    trim_spaces_and_tabs_from_both_ends();
+    trim_empty_vector_does_nothing();
+    trim_non_space_vector_does_nothing();
+    trim_all_space_vector_removes_everything();
+    trim_vector_spaces_from_left();
+    trim_vector_spaces_from_right();
+    trim_vector_spaces_from_both_ends();
+    trim_vector_spaces_from_left_only();
+    trim_vector_spaces_from_right_only();
+    trim_vector_copy_empty_string_does_nothing();
+    trim_vector_copy_non_space_string_does_nothing();
+    trim_vector_copy_all_space_string_removes_everything();
+    trim_vector_copy_spaces_from_left();
+    trim_vector_copy_spaces_from_right();
+    trim_vector_copy_spaces_from_both_ends();
+    trim_vector_copy_spaces_from_left_only();
+    trim_vector_copy_spaces_from_right_only();
 }
