0c02192a5e3497992400540ac4706e853d8b2fb0
Anthony Williams <anthony@justsoftwaresolutions.co.uk>
2017-09-15 08:42:56 +0100

    Reformatted

--- string_trim.hpp
+++ string_trim.hpp
@@ -5,22 +5,29 @@
 #include <iterator>
 #include <algorithm>
 
-template<typename Predicate,typename CharacterType,bool = std::is_invocable_v<Predicate,CharacterType>>
-struct is_whitespace_predicate_impl{
-    static constexpr bool value=false;
+template <
+    typename Predicate, typename CharacterType,
+    bool= std::is_invocable_v<Predicate, CharacterType>>
+struct is_whitespace_predicate_impl {
+    static constexpr bool value= false;
 };
 
-template<typename Predicate,typename CharacterType>
-struct is_whitespace_predicate_impl<Predicate,CharacterType,true>{
-    static constexpr bool value=std::is_convertible_v<decltype(std::declval<Predicate>()(std::declval<CharacterType>())),bool>;
+template <typename Predicate, typename CharacterType>
+struct is_whitespace_predicate_impl<Predicate, CharacterType, true> {
+    static constexpr bool value= std::is_convertible_v<
+        decltype(std::declval<Predicate>()(std::declval<CharacterType>())),
+        bool>;
 };
 
-template<typename Predicate,typename CharacterType>
-constexpr bool is_whitespace_predicate = is_whitespace_predicate_impl<Predicate,CharacterType>::value;
+template <typename Predicate, typename CharacterType>
+constexpr bool is_whitespace_predicate=
+    is_whitespace_predicate_impl<Predicate, CharacterType>::value;
 
 template <typename Iterator>
 typename std::enable_if<
-    std::is_same_v<char, std::remove_cv_t<typename std::iterator_traits<Iterator>::value_type> >,
+    std::is_same_v<
+        char,
+        std::remove_cv_t<typename std::iterator_traits<Iterator>::value_type>>,
     Iterator>::type
 find_first_non_whitespace(Iterator begin, Iterator end) {
     auto pos= begin;
@@ -31,7 +38,9 @@ find_first_non_whitespace(Iterator begin, Iterator end) {
 
 template <typename Iterator>
 typename std::enable_if<
-    std::is_same_v<wchar_t, std::remove_cv_t<typename std::iterator_traits<Iterator>::value_type> >,
+    std::is_same_v<
+        wchar_t,
+        std::remove_cv_t<typename std::iterator_traits<Iterator>::value_type>>,
     Iterator>::type
 find_first_non_whitespace(Iterator begin, Iterator end) {
     auto pos= begin;
@@ -40,48 +49,61 @@ find_first_non_whitespace(Iterator begin, Iterator end) {
     return pos;
 }
 
-template<typename Iterator,typename CharacterList>
+template <typename Iterator, typename CharacterList>
 typename std::enable_if<
-    !is_whitespace_predicate<CharacterList, typename std::iterator_traits<Iterator>::value_type>,
+    !is_whitespace_predicate<
+        CharacterList, typename std::iterator_traits<Iterator>::value_type>,
     Iterator>::type
-find_first_non_whitespace(Iterator begin,Iterator end,CharacterList&& space_chars){
-    auto pos=begin;
-    auto begin_of_chars=std::begin(space_chars);
-    auto end_of_chars=std::end(space_chars);
-    for(;(pos!=end) && (std::find(begin_of_chars,end_of_chars,*pos)!=end_of_chars);++pos);
+find_first_non_whitespace(
+    Iterator begin, Iterator end, CharacterList &&space_chars) {
+    auto pos= begin;
+    auto begin_of_chars= std::begin(space_chars);
+    auto end_of_chars= std::end(space_chars);
+    for(; (pos != end) &&
+          (std::find(begin_of_chars, end_of_chars, *pos) != end_of_chars);
+        ++pos)
+        ;
     return pos;
 }
 
-template<typename Iterator,typename Predicate>
+template <typename Iterator, typename Predicate>
 typename std::enable_if<
-    is_whitespace_predicate<Predicate, typename std::iterator_traits<Iterator>::value_type>,
+    is_whitespace_predicate<
+        Predicate, typename std::iterator_traits<Iterator>::value_type>,
     Iterator>::type
-find_first_non_whitespace(Iterator begin,Iterator end,Predicate&& is_white_space){
-    auto pos=begin;
-    for(;(pos!=end) && is_white_space(*pos);++pos);
+find_first_non_whitespace(
+    Iterator begin, Iterator end, Predicate &&is_white_space) {
+    auto pos= begin;
+    for(; (pos != end) && is_white_space(*pos); ++pos)
+        ;
     return pos;
 }
 
-
-template<typename Iterator>
+template <typename Iterator>
 typename std::enable_if<
-    std::is_same_v<char, std::remove_cv_t<typename std::iterator_traits<Iterator>::value_type> >,
+    std::is_same_v<
+        char,
+        std::remove_cv_t<typename std::iterator_traits<Iterator>::value_type>>,
     Iterator>::type
-find_last_non_whitespace(Iterator begin,Iterator end){
+find_last_non_whitespace(Iterator begin, Iterator end) {
     auto endpos= std::make_reverse_iterator(end);
     for(auto start= std::make_reverse_iterator(begin);
-        (endpos != start) && std::isspace(*endpos); ++endpos);
+        (endpos != start) && std::isspace(*endpos); ++endpos)
+        ;
     return endpos.base();
 }
 
-template<typename Iterator>
+template <typename Iterator>
 typename std::enable_if<
-    std::is_same_v<wchar_t, std::remove_cv_t<typename std::iterator_traits<Iterator>::value_type> >,
+    std::is_same_v<
+        wchar_t,
+        std::remove_cv_t<typename std::iterator_traits<Iterator>::value_type>>,
     Iterator>::type
-find_last_non_whitespace(Iterator begin,Iterator end){
+find_last_non_whitespace(Iterator begin, Iterator end) {
     auto endpos= std::make_reverse_iterator(end);
     for(auto start= std::make_reverse_iterator(begin);
-        (endpos != start) && std::iswspace(*endpos); ++endpos);
+        (endpos != start) && std::iswspace(*endpos); ++endpos)
+        ;
     return endpos.base();
 }
 
@@ -99,18 +121,15 @@ Iterator find_last_non_whitespace(
     return endpos.base();
 }
 
-template<typename Container>
-void trim_left(Container& s){
-    s.erase(s.begin(),find_first_non_whitespace(s.begin(),s.end()));
+template <typename Container> void trim_left(Container &s) {
+    s.erase(s.begin(), find_first_non_whitespace(s.begin(), s.end()));
 }
 
-template<typename Container>
-void trim_right(Container& s){
-    s.erase(find_last_non_whitespace(s.begin(),s.end()),s.end());
+template <typename Container> void trim_right(Container &s) {
+    s.erase(find_last_non_whitespace(s.begin(), s.end()), s.end());
 }
 
-template<typename Container>
-void trim(Container& s){
+template <typename Container> void trim(Container &s) {
     trim_left(s);
     trim_right(s);
 }
@@ -137,9 +156,9 @@ void trim_left(Container &s, typename Container::value_type *space_chars) {
         s, std::basic_string_view<typename Container::value_type>(space_chars));
 }
 
-template<typename Container,typename CharacterList>
-void trim_right(Container& s,CharacterList&& space_chars){
-    s.erase(find_last_non_whitespace(s.begin(),s.end(),space_chars),s.end());
+template <typename Container, typename CharacterList>
+void trim_right(Container &s, CharacterList &&space_chars) {
+    s.erase(find_last_non_whitespace(s.begin(), s.end(), space_chars), s.end());
 }
 
 template <typename Container>
@@ -164,27 +183,25 @@ trim_left(Container &s, Predicate &&predicate) {
         s.begin(), find_first_non_whitespace(s.begin(), s.end(), predicate));
 }
 
-template<typename Container>
-std::remove_reference_t<Container> trim_copy_left(Container&& s){
-    using string_type=std::remove_reference_t<Container>;
-    return string_type(find_first_non_whitespace(s.begin(),s.end()),s.end());
+template <typename Container>
+std::remove_reference_t<Container> trim_copy_left(Container &&s) {
+    using string_type= std::remove_reference_t<Container>;
+    return string_type(find_first_non_whitespace(s.begin(), s.end()), s.end());
 }
 
-template<typename Container>
-std::remove_reference_t<Container> trim_copy_right(Container&& s){
-    using string_type=std::remove_reference_t<Container>;
-    return string_type(s.begin(),find_last_non_whitespace(s.begin(),s.end()));
+template <typename Container>
+std::remove_reference_t<Container> trim_copy_right(Container &&s) {
+    using string_type= std::remove_reference_t<Container>;
+    return string_type(s.begin(), find_last_non_whitespace(s.begin(), s.end()));
 }
 
-template<typename Container>
-std::remove_reference_t<Container> trim_copy(Container&& s){
-    using string_type=std::remove_reference_t<Container>;
-    auto const begin=s.begin();
-    auto const end=s.end();
-    auto const copy_begin=find_first_non_whitespace(begin,end);
-    return (copy_begin==end)?string_type(end,end):
-        string_type(
-        copy_begin,
-        find_last_non_whitespace(begin,end));
+template <typename Container>
+std::remove_reference_t<Container> trim_copy(Container &&s) {
+    using string_type= std::remove_reference_t<Container>;
+    auto const begin= s.begin();
+    auto const end= s.end();
+    auto const copy_begin= find_first_non_whitespace(begin, end);
+    return (copy_begin == end) ?
+               string_type(end, end) :
+               string_type(copy_begin, find_last_non_whitespace(begin, end));
 }
-
